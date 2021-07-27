fb861ca6e7112240a771be97478afc2d88c2c25e
hoang@mission.plus <hoang@mission.plus>
2021-07-27 18:34:00 +0700

    base

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./App.scss";
+import NavbarComponent from "./component/Navbar";
+import Price from "./component/Price";
+import Order from "./component/Order";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <NavbarComponent />
+        <Route exact path="/prices" component={() => <Price />} />
+        <Route exact path="/orders" component={() => <Order />} />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/component/Navbar.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { Fragment } from "react";
+import SVG from "react-inlinesvg/esm";
+import Logo from "../image/logo.svg";
+import { Button, Navbar } from "rtc-design-prototype";
+import { Link } from "react-router-dom";
+const NavbarComponent = () => {
+  return (
+    <Fragment>
+      <Navbar className="dark_version">
+        <Navbar.Group>
+          <Navbar.Heading>
+            <Link to="/">
+              {" "}
+              <SVG src={Logo} />
+            </Link>
+          </Navbar.Heading>
+          <ul>
+            <li>
+              <Link to="/prices">Prices</Link>
+            </li>
+            <li>
+              <Link to="/orders">Orders</Link>
+            </li>
+          </ul>
+        </Navbar.Group>
+        <Navbar.Group className="nav-right" align={"right"}>
+          <Button minimal icon="caret-down" />
+        </Navbar.Group>
+      </Navbar>
+    </Fragment>
+  );
+};
+
+export default NavbarComponent;
